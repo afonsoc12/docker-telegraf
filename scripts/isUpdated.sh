119ac3f41de6ce754420ad080c5d65addde8bef3
Afonso Costa <afonsoc1997@gmail.com>
2022-07-23 15:04:27 +0100

    Fix regex grep

--- scripts/isUpdated.sh
+++ scripts/isUpdated.sh
@@ -1,136 +1,17 @@
 #!/bin/bash
 
-# Check exiting tools
-echo "curl: $(curl --version)"
-echo "grep: $(grep --version)"
-echo "jq: $(jq --version)"
-echo "sort: $(sort --version)"
-echo "head: $(head --version)"
-
-echo $(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r | \
-                    head -n1)
-
-echo $(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]')
-echo $(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$')
-
-echo $(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r)
-
-echo $(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r | \
-                    head -n1)
-echo "=============="
-
-a=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r | \
-                    head -n1)
-
-b=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]')
-c=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$')
-
-d=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r)
-
-e=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r | \
-                    head -n1)
-
-echo $a
-echo $b
-echo $c
-echo $d
-echo $e
-
-echo "+++++++++++"
 function retrieve_latest_image () {
     image=$1
     local version=$(curl -s \
                 -H "Accept: application/json" \
                 https://hub.docker.com/v2/repositories/$image/tags | \
                     jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
+                    grep -P '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
                     sort -n -r | \
                     head -n1)
     echo $version
 }
 
-a=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r | \
-                    head -n1)
-
-b=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]')
-c=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$')
-
-d=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r)
-
-e=$(curl -s \
-                -H "Accept: application/json" \
-                https://hub.docker.com/v2/repositories/library/telegraf/tags | \
-                    jq -r '.["results"][]["name"]' | \
-                    grep -E '^(\d+\.)?(\d+\.)?(\*|\d+)$' | \
-                    sort -n -r | \
-                    head -n1)
-
-echo $a
-echo $b
-echo $c
-echo $d
-echo $e
 ext_tag=$(retrieve_latest_image 'library/telegraf')
 
 echo "**** External release is $ext_tag"
